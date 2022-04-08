8eeb3a24712a63a6d96ef6c5e07ef6d00137ef24
JOY <joybosu@gmail.com>
2022-04-08 20:44:07 +0530

    [Joy] Adding 1~st Program

---
+++ ArithmaticOpr.js
@@ -0,0 +1,15 @@
+let a=10;
+let b=36;
+let c=23;
+
+let d= a+b*c;
+console.log("Arithmatic Opration No-1 :"+d);
+
+let e=c+a/b;
+console.log("Arithmatic opration No-2:" +e);
+
+let f=a%b+c;
+console.log("Arithmatic opration No-3:" +f);
+
+let g=a*b+c;
+console.log("Arithmatic opration No-4:" +f);
