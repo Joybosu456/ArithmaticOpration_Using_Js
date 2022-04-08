36b4077952c7c6d150062a86d50f40f50f4888f1
JOY <joybosu@gmail.com>
2022-04-08 21:05:04 +0530

    [Joy] adding Random number Program

---
+++ RandomNum.js
@@ -0,0 +1,19 @@
+let x=Math.floor(Math.random()*110)+11;
+console.log(x);
+
+let y=Math.floor(Math.random()*168)+11;
+console.log(y);
+
+let z=Math.floor(Math.random()*166)+11;
+console.log(y);
+
+let za=Math.floor(Math.random()*156)+10;
+console.log(y);
+
+let c=Math.floor(Math.random()*155)+33;
+ console.log(c);
+
+let v=x+y+z+za+c;
+console.log(v);
+let l=v/5;
+console.log(l);
